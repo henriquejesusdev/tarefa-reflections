fe3a99425d1854272e102047ab12c0c6a05437d7
Henrique Jesus <henrique.jesus128@gmail.com>
2026-07-02 02:04:37 -0300

    first commit

---
+++ src/main/java/br/com/tarefa/annotation/Tabela.java
@@ -0,0 +1,4 @@
+package br.com.tarefa.annotation;
+import java.lang.annotation.*;
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Tabela{String value();}
---
+++ src/main/java/br/com/tarefa/model/Cliente.java
@@ -0,0 +1,4 @@
+package br.com.tarefa.model;
+import br.com.tarefa.annotation.Tabela;
+@Tabela("clientes")
+public class Cliente{}
