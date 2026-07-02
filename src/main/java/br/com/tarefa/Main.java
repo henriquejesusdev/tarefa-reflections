d67399f19d66d2233616facb03903dd425112060
Henrique Jesus <henrique.jesus128@gmail.com>
2026-07-02 02:05:16 -0300

    fix formaytação do codigo

--- src/main/java/br/com/tarefa/Main.java
+++ src/main/java/br/com/tarefa/Main.java
@@ -1,3 +1,16 @@
 package br.com.tarefa;
-import br.com.tarefa.annotation.Tabela;import br.com.tarefa.model.*;
-public class Main{public static void main(String[]args){Class<?>[] cs={Cliente.class,Produto.class};for(Class<?> c:cs){if(c.isAnnotationPresent(Tabela.class)){Tabela t=c.getAnnotation(Tabela.class);System.out.println(c.getSimpleName()+" -> "+t.value());}}}}
+
+import br.com.tarefa.annotation.Tabela;
+import br.com.tarefa.model.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Class<?>[] cs = { Cliente.class, Produto.class };
+        for (Class<?> c : cs) {
+            if (c.isAnnotationPresent(Tabela.class)) {
+                Tabela t = c.getAnnotation(Tabela.class);
+                System.out.println(c.getSimpleName() + " -> " + t.value());
+            }
+        }
+    }
+}
